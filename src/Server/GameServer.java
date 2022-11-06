2561bd26b2edb0a23b906e2f1e08ac2da002cc4b
손여산 <mryosan1004@hansung.ac.kr>
2022-11-06 21:14:22 +0900

    Server 수정

--- src/Server/GameServer.java
+++ src/Server/GameServer.java
@@ -1,5 +1,114 @@
 package Server;
 
-public class GameServer {
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.*;
+import java.awt.event.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
+public class GameServer extends JFrame {
+
+    //필요한 변수들 선언하기
+    private static final long serialVersionUID = 1L;
+    private JPanel contentPane;
+    JTextArea textArea;
+    private JTextField txtPortNumber;
+
+    private ServerSocket socket; // 서버소켓
+    private Socket client_socket; // accept() 에서 생성된 client 소켓
+    private Vector UserVec = new Vector(); // 연결된 사용자를 저장할 벡터
+    private static final int BUF_LEN = 128; // Windows 처럼 BUF_LEN 을 정의
+
+    public String word[] = {"새", "나무", "사람"}; //단어 서버에서 어떤 방식으로 뿌릴지 정하기,,
+
+    public GameServer() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 338, 440);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+
+        JScrollPane scrollPane = new JScrollPane();
+        scrollPane.setBounds(12, 10, 300, 298);
+        contentPane.add(scrollPane);
+
+        textArea = new JTextArea();
+        textArea.setEditable(false);
+        scrollPane.setViewportView(textArea);
+
+        JLabel lblNewLabel = new JLabel("Port Number");
+        lblNewLabel.setBounds(13, 318, 87, 26);
+        contentPane.add(lblNewLabel);
+
+        txtPortNumber = new JTextField();
+        txtPortNumber.setHorizontalAlignment(SwingConstants.CENTER);
+        txtPortNumber.setText("30000");
+        txtPortNumber.setBounds(112, 318, 199, 26);
+        contentPane.add(txtPortNumber);
+        txtPortNumber.setColumns(10);
+
+        JButton btnServerStart = new JButton("Server Start");
+        btnServerStart.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    socket = new ServerSocket(Integer.parseInt(txtPortNumber.getText()));
+                } catch (NumberFormatException | IOException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                }
+                AppendText("Chat Server Running..");
+                btnServerStart.setText("Chat Server Running..");
+                btnServerStart.setEnabled(false); // 서버를 더이상 실행시키지 못 하게 막는다
+                txtPortNumber.setEnabled(false); // 더이상 포트번호 수정못 하게 막는다
+                //AcceptServer accept_server = new AcceptServer();
+                //accept_server.start();
+            }
+        });
+        btnServerStart.setBounds(12, 356, 300, 35);
+        contentPane.add(btnServerStart);
+    } //GameServer 끝
+
+    /* class AcceptServer extends Thread {
+        @SuppressWarnings("unchecked")
+        public void run() {
+            while(true) {
+                try {
+                    AppendText("Waiting new client...");
+                    client_socket = socket.accept(); //accept가 일어날때까지 무한대기
+                    AppendText("새로운 참가자 from " + client_socket);
+                    // User 당 하나씩 Thread 생성
+                    UserService new_user = new UserService(client_socket);
+                    UserVec.add(new_user); // 새로운 참가자 배열에 추가
+                    new_user.start(); // 만든 객체의 스레드 실행
+                    AppendText("현재 참가자 수 " + UserVec.size());
+                } catch (IOException e) {
+                    AppendText("accept() error");
+                    // System.exit(0);
+                }
+            }
+        }
+    } */
+
+    public void AppendText(String str) {
+        // textArea.append("사용자로부터 들어온 메세지 : " + str+"\n");
+        textArea.append(str + "\n");
+        textArea.setCaretPosition(textArea.getText().length());
+    }
+
+    public static void main(String[] args) {
+        //실행시키는 코드
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                    try {
+                        GameServer frame = new GameServer();
+                        frame.setVisible(true);
+                    } catch(Exception e) {
+                        e.printStackTrace();
+                    }
+            }
+        });
+    } //main끝
 }
